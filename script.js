52806748b44d8527d7b263938915e5f04e4727e3
Manoj Reddy <kakimanojreddy22@gmail.com>
2024-07-29 16:55:13 +0530

    Todo form created with CURD operation

---
+++ script.js
@@ -0,0 +1,64 @@
+document.getElementById('todoform').addEventListener('submit', function(e){
+    e.preventDefault(); 
+    const todoInputElement = document.getElementById('todoitem');
+    const todo = todoInputElement.value.trim();
+    if(todo !== ''){
+        addTodoItem(todo);
+        todoInputElement.value = ''; // Reset input value
+    } else {
+        alert('Please enter a todo item');
+    }
+});
+
+function addTodoItem(todo) {
+    const todolist = document.getElementById('todolist');
+    const list_item = document.createElement('li');
+    list_item.classList.add('flex','ml-3' ,'mt-5','mb-5');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.classList.add('ml-2', 'mr-3');
+    checkbox.addEventListener('change',function(e){
+        list_item.classList.toggle('line-through');
+        
+    })
+
+    const label = document.createElement('label');
+    label.classList.add('text-gray-700'); 
+    label.textContent = todo;
+
+    const editBtn = document.createElement('button');
+    editBtn.innerHTML = '<i class="fas fa-edit"></i>';
+    editBtn.classList.add('ml-3', 'bg-blue-500', 'hover:bg-blue-700', 'text-white', 'font-bold', 'py-2', 'px-4', 'rounded');
+    editBtn.addEventListener('click',function(e){
+        e.preventDefault();
+        const confirmEdit = confirm('Are you sure you want to edit this item?');
+        if(confirmEdit){
+            const newinput = prompt('Enter new todo');
+            if(newinput !== null){
+                label.textContent = newinput;
+            }
+        }
+
+    })
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteBtn.classList.add('ml-3', 'bg-red-500', 'hover:bg-red-700', 'text-white', 'font-bold', 'py-2', 'px-4', 'rounded');
+    deleteBtn.addEventListener('click',function(e){
+        e.preventDefault();
+        const confirmDelete = confirm('Are you sure you want to delete this item?');
+        if(confirmDelete){
+            list_item.remove();
+        }
+        
+    })
+
+
+
+    list_item.appendChild(checkbox);
+    list_item.appendChild(label);
+    list_item.appendChild(editBtn);
+    list_item.appendChild(deleteBtn);
+    todolist.appendChild(list_item);   
+}
